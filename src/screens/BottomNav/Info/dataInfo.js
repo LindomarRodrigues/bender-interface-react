2b9e216b78dc29dfdbde14d8402bdb539ca91f35
Rafael Silva <rafaelrst15@gmail.com>
2022-04-16 18:38:00 -0300

    Criação da tela dinamica de informações

---
+++ src/screens/BottomNav/Info/dataInfo.js
@@ -0,0 +1,33 @@
+export default data = [
+    {
+      "nome":"Informações Gerais",
+      "api": "https://bender-api-uft.herokuapp.com/listar_emails_professores",
+      "file": true
+    },
+    {
+      "nome":"E-mail dos professores",
+      "api": "https://bender-api-uft.herokuapp.com/listar_emails_professores",
+      "file": false
+   },
+   {
+      "nome":"Lattes dos docentes",
+   },
+   {
+      "nome":"Matriz curricular",
+   },
+   {
+      "nome":"Contatos da Coordenação",
+   },
+   {
+      "nome":"Regimento Academico e Geral",
+   },
+   {
+      "nome":"Calendário Acadêmico",
+   },
+   {
+    "nome":"PPC",
+   },
+   {
+    "nome":"Grupos",
+   }
+    ]
