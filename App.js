24ac64214f10c1dea01ab049bc58b7c25fe1bd35
Mateus-Arauj <mateus9509595091@gmail.com>
2022-04-19 09:42:31 -0300

    R71-Alterar fundo na navegação entre telas

--- App.js
+++ App.js
@@ -4,15 +4,21 @@ import Login from "./src/screens/Login";
 import Cadastro from "./src/screens/Cadastro";
 import BottomNav from "./src/screens/BottomNav";
 import ResultHorarios from "./src/screens/BottomNav/Horario/ResultHorarios";
-import InfoScreen from "./src/screens/BottomNav/Info/InformaçõesGerais";
+import InfoScreen from "./src/screens/BottomNav/Info/InfoScreen";
 import {createStackNavigator} from "@react-navigation/stack";
 import {NavigationContainer} from "@react-navigation/native";
 import "react-native-gesture-handler";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createStackNavigator();
-
+const forFade = ({ current, closing }) => ({
+        cardStyle: {
+          opacity: current.progress,
+        },
+      });
+      
 export default function App() {
+    //Autenticaçao
     const [encJwt, setEncJwt] = useState(null);
     const [carregando, setCarregando] = useState(true);
     AsyncStorage.getItem('@enc_jwt').then(r => {
@@ -25,38 +31,40 @@ export default function App() {
     if (carregando) {
         return null;
     }
+
+    
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName={encJwt === null ? "BoasVindas" : "BottomNav"}>
                 <Stack.Screen
                     name="BoasVindas"
                     component={BoasVindas}
-                    options={{headerShown: false}}
+                    options={{headerShown: false, cardStyleInterpolator: forFade}}
                 />
                 <Stack.Screen
                     name="Login"
                     component={Login}
-                    options={{headerShown: false}}
+                    options={{headerShown: false, cardStyleInterpolator: forFade}}
                 />
                 <Stack.Screen
                     name="Cadastro"
                     component={Cadastro}
-                    options={{headerShown: false}}
+                    options={{headerShown: false, cardStyleInterpolator: forFade}}
                 />
                 <Stack.Screen
                     name="BottomNav"
                     component={BottomNav}
-                    options={{headerShown: false}}
+                    options={{headerShown: false, cardStyleInterpolator: forFade}}
                 />
                 <Stack.Screen
                     name="ResultHorarios"
                     component={ResultHorarios}
-                    options={{headerShown: false}}
+                    options={{headerShown: false, cardStyleInterpolator: forFade}}
                 />
                 <Stack.Screen
                     name="InfoScreen"
                     component={InfoScreen}
-                    options={{headerShown: false}}
+                    options={{headerShown: false, cardStyleInterpolator: forFade}}
                 />
             </Stack.Navigator>
         </NavigationContainer>
